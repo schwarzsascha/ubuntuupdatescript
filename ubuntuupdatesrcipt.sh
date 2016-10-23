6a2faf441c60342fccc5b7732e137b75b27ce248
saschas <gedankenegel@gmail.com>
2016-10-23 15:16:24 +0200

    code eine Zeile verkürzt

--- ubuntuupdatesrcipt.sh
+++ ubuntuupdatesrcipt.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get dist-upgrade
+sudo apt-get update && sudo apt-get dist-upgrade
 sudo apt-get autoremove
 sudo apt-get autoclean
 read -p "reboot (r) oder ende (e)? Geben Sie r oder e ein und die Eingabetaste ...  " kommando; if [ $kommando = 'r' ]; then reboot; elif [ $kommando = 'e' ]; then exit; fi
